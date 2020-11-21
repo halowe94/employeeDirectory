f5a978479da267e8a3d2e2e75a72fc4b09421465
halowe <haleylowe0@gmail.com>
2020-11-20 19:18:37 -0800

    imported react for components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import EmployeeTable from "./components/EmployeeTable";
+
 import { EmployeeContext } from "./components/EmployeeContext";
 
 function App() {
   const [employees, setEmployees] = usetState([])
+  const [visibleEmployees, setVisibleEmployees] = useState([])
 
   return (
     <div className="App">
-      <EmployeeContext.Provider></EmployeeContext.Provider>
+      <EmployeeContext.Provider value={{employees, setEmployees, visibleEmployees, setVisibleEmployees}}></EmployeeContext.Provider>
     </div>
   );
 }
