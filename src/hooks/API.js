022d4e011b896f3df87bef5df5a9cf21af87235a
halowe <haleylowe0@gmail.com>
2020-11-23 19:49:07 -0800

    try catch added to getEmployees function

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -2,6 +2,23 @@ import {useEffect, useContext} from 'react';
 import {EmployeeContext} from "../components/EmployeeContext"
 import axios from 'axios'
 
+//useGet function
 export function useGet(url) {
     const {employees, setEmployees, visibleEmployees, setVisibleEMployees}  = use(EmployeeContext)
+
+    //useEffect function
+    useEffect(() => {
+        async function getEmployees() {
+            try {
+
+            } catch (error) {
+                console.log("error with API:", error)
+            }
+        }
+        //call getEmployees function
+        getEmployees()
+
+    })
+    //end useEffect function
 }
+//end useGet function
