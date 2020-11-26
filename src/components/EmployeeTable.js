82bd6c4a944458c47c0352dcdf942990418a86ea
halowe <haleylowe0@gmail.com>
2020-11-25 22:43:01 -0800

    added to tr

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,23 +1,32 @@
 import React, {useState, useContext} from 'react';
 import { EmployeeContext } from './EmployeeContext'
+import Button from './Button'
 
 function EmployeeTable() {
 
     const [url] = useState()
     const { sortFunc } = useGet(url)
     const { visibleEmployees } = useContext(EmployeeContext)
+
     return (
         <table>
             <thead>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>Last Name</td>
+                    <td>UID</td>
+                    <td>Gender</td>
+                    <td>E-mail</td>
                 </tr>
             </thead>
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
         </table>
 
     )
-}
+};
 
-export default Table; 
+export default EmployeeTable; 
