c8c5791f420dd3840096f2d342acfd9fc10b72be
halowe <haleylowe0@gmail.com>
2020-11-27 17:01:07 -0800

    created table body and objects to be returned in data cells

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react';
 import { EmployeeContext } from './EmployeeContext'
 import Button from './Button'
+import {useGet} from '../hooks/API';
 
 function EmployeeTable() {
 
@@ -12,17 +13,27 @@ function EmployeeTable() {
         <table>
             <thead>
                 <tr>
+                    <td onClick={() => sortFunc('age')}><Button>First Name</Button></td>
                     <td>Last Name</td>
                     <td>UID</td>
                     <td>Gender</td>
                     <td>E-mail</td>
+                    <td onClick={() => sortFunc('age')}><Button>Age</Button></td>
                 </tr>
             </thead>
             <tbody>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                {visibleEmployees.map(employee => {
+                    return (
+                        <tr key={employee.login.uuid}>
+                            <td>{employee.name.first}</td>
+                            <td>{employee.name.last}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.dob.age}</td>
+                        </tr>
+                    )
+                })}
+                
             </tbody>
         </table>
 
