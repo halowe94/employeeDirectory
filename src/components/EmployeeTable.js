b866c5ffd065a6fcaf01b452a8b6fc008a982d91
halowe <haleylowe0@gmail.com>
2020-12-08 18:18:19 -0800

    created filter by first name in EmployeeTable.js

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,44 +1,67 @@
-import React, {useState, useContext} from 'react';
-import { EmployeeContext } from './EmployeeContext'
-import Button from './Button'
-import {useGet} from '../hooks/API';
+import React, { useState, useContext } from "react";
+import { EmployeeContext } from "./EmployeeContext";
+import Button from "./Button";
+import { useGet } from "../hooks/API";
 
 //begin employee table function
 function EmployeeTable() {
+  const [url] = useState("https://randomuser.me/api/?results=10");
+  const { sortFunc } = useGet(url);
+  const {
+    visibleEmployees,
+    setVisibleEmployees,
+    setEmployees,
+    employees,
+  } = useContext(EmployeeContext);
 
-    const [url] = useState("https://randomuser.me/api/?results=10")
-    const { sortFunc } = useGet(url)
-    const { visibleEmployees } = useContext(EmployeeContext)
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    if (e.target.value === "") {
+      setVisibleEmployees(employees);
+    } else {
+      const filteredList = visibleEmployees.filter((person) => {
+        console.log(person.name.first);
+        return person.name.first.includes(e.target.value);
+      });
+      console.log(filteredList);
+      setVisibleEmployees(filteredList);
+    }
+  };
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <td onClick={() => sortFunc('name')}><Button>First Name</Button></td>
-                    <td>Last Name</td>
-                    <td>Gender</td>
-                    <td>E-mail</td>
-                    <td onClick={() => sortFunc('age')}><Button>Age</Button></td>
-                </tr>
-            </thead>
-            <tbody>
-                {visibleEmployees.map(employee => {
-                    return (
-                        <tr key={employee.login.uuid}>
-                            <td>{employee.name.first}</td>
-                            <td>{employee.name.last}</td>
-                            <td>{employee.gender}</td>
-                            <td>{employee.email}</td>
-                            <td>{employee.dob.age}</td>
-                        </tr>
-                    )
-                })}
-                
-            </tbody>
-        </table>
-
-    )
-};
+  return (
+    <>
+      <input name="searchInput" onChange={handleChange} />
+      <table className="mx-auto">
+        <thead>
+          <tr>
+            <td onClick={() => sortFunc("name")}>
+              <Button>First Name</Button>
+            </td>
+            <td>Last Name</td>
+            <td>Gender</td>
+            <td>E-mail</td>
+            <td onClick={() => sortFunc("age")}>
+              <Button>Age</Button>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {visibleEmployees.map((employee) => {
+            return (
+              <tr key={employee.login.uuid}>
+                <td>{employee.name.first}</td>
+                <td>{employee.name.last}</td>
+                <td>{employee.gender}</td>
+                <td>{employee.email}</td>
+                <td>{employee.dob.age}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
 //end employee table function
 
-export default EmployeeTable; 
+export default EmployeeTable;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 //import Search from './SearchBar'
 
 function NavBar() {
-    return (
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-       <h2>Employee Directory</h2>
-       <div>
-           <h3>Employee Search:</h3>
-           {/* Search bar here */}
-       </div>
+      <h2>Employee Directory</h2>
+      <div>
+        <h3>Search By First Name:</h3>
+        {/* Search bar here */}
+      </div>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
